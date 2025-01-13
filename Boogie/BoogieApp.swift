573d7820d43f5eb2c4a0dcc5c3e90eabf21b1c09
Tim <Timothy.hays@bc.edu>
2025-01-13 18:36:16 -0500

    Initial Commit

---
+++ Boogie/BoogieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoogieApp.swift
+//  Boogie
+//
+//  Created by Tim Hays on 1/13/25.
+//
+
+import SwiftUI
+
+@main
+struct BoogieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
